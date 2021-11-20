ab889b90a2ddb0bb57e0dd3c58446aa4877b4b05
Krzysztof Baranski <krisbaranski@gmail.com>
2021-11-20 22:50:44 +0100

    feat: Add page-3, adjust <Layout>

---
+++ src/pages/posts/page-3.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../../components/layout"
+import { Link } from "gatsby"
+
+export default () => (
+  <Layout>
+    <h1>Hello from Page 3</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
