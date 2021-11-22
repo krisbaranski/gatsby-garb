638e0f4babd74e8a9676c1a197df9793538a35e7
Krzysztof Baranski <krisbaranski@gmail.com>
2021-11-22 23:09:25 +0100

    Add StaticQuery imageData to Page-3

--- src/pages/posts/page-3.js
+++ src/pages/posts/page-3.js
@@ -1,10 +1,49 @@
 import React from "react"
+import { graphql, StaticQuery, Link } from "gatsby"
+
 import Layout from "../../components/layout"
-import { Link } from "gatsby"
+
+const getImageData = graphql`
+  {
+    allFile {
+      edges {
+        node {
+          relativePath
+          id
+          size
+        }
+      }
+    }
+  }
+`
 
 export default () => (
   <Layout>
     <h1>Hello from Page 3</h1>
+    <h3>Image File Data</h3>
+    <StaticQuery
+      query={getImageData}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>ID</th>
+              <th>Size</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.id}</td>
+                <td>{node.size}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
